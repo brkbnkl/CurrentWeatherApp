90e0bf3547c4593f81e0f24deef079b259b79474
Berk Benkel <brkbnkl@gmail.com>
2024-04-04 15:04:23 +0300

    edit city info

--- WeatherApp/src/components/CityInfo.jsx
+++ WeatherApp/src/components/CityInfo.jsx
@@ -6,15 +6,15 @@ const CityInfo = ({ city }) => {
         <div className="oldList">
             <p className="oldTime">{new Date().toLocaleDateString()}</p>
             <p className="oldCity">{city.name}, {city.country}</p>
-            <p className="oldTemp">{Math.round(city.temp)}°C</p>
-            <p className="oldFeelsLike">HİSSEDİLEN {Math.floor(city.feels_like)}°C</p>
-            <p className="oldHumidity">NEM : {city.humidity} %</p>
-            <p className="oldDesc">HAVA {city.description.toUpperCase()}</p>
-            <p className="oldMinmax">EN DÜŞÜK {Math.round(city.temp_min)}°C / EN YÜKSEK {Math.round(city.temp_max)}°C</p>
-            <p className="oldWind2">RÜZGAR : {city.wind_speed} M/S</p>
-            <p className="oldPressure">BASINÇ : {city.pressure} Mbar</p>
-            <p className="oldSunrise">GÜN DOĞUMU : {formatTime(city.sunrise)}</p>
-            <p className="oldSunset">GÜN BATIMI : {formatTime(city.sunset)}</p>
+            <p className="oldTemp">TEMP: {Math.round(city.temp)}°C</p>
+            <p className="oldFeelsLike">FEELS LIKE: {Math.floor(city.feels_like)}°C</p>
+            <p className="oldHumidity">HUMIDITY: {city.humidity} %</p>
+            <p className="oldDesc">WEATHER: {city.description.toUpperCase()}</p>
+            <p className="oldMinmax">MIN TEMP: {Math.round(city.temp_min)}°C / MAX TEMP: {Math.round(city.temp_max)}°C</p>
+            <p className="oldWind2">WIND: {city.wind_speed} M/S</p>
+            <p className="oldPressure">PRESSURE: {city.pressure} Mbar</p>
+            <p className="oldSunrise">SUN RISE: {formatTime(city.sunrise)}</p>
+            <p className="oldSunset">SUN SET: {formatTime(city.sunset)}</p>
         </div>
     );
 };
